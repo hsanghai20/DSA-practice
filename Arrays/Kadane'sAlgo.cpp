de18be0502fa84290886261439d1539635fd2127
hsanghai20 <harshsanghai2000@gmail.com>
2020-06-30 18:56:26 +0530

    kadane algorithm

---
+++ Arrays/Kadane'sAlgo.cpp
@@ -0,0 +1,11 @@
+// Maax contigous subarray
+
+int maxSubArray(int A[], int n) {
+        int ans=A[0],i,j,sum=0;
+        for(i=0;i<n;i++){
+            sum+=A[i];
+            ans=max(sum,ans);
+            sum=max(sum,0);
+        }
+        return ans;
+    }
