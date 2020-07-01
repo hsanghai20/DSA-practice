c47fc179abd27679d080fff9a6decdc24417a8a2
hsanghai20 <harshsanghai2000@gmail.com>
2020-07-01 16:19:04 +0530

    Buy and sell stock

---
+++ Arrays/BuyAndSellStock.cpp
@@ -0,0 +1,15 @@
+//If you were only permitted to complete at most one transaction 
+//(i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+// function
+int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int mi=INT_MAX;
+        int ans=0;
+        for (int i=0;i<n;i++)
+        {
+            mi=min(mi,prices[i]);
+            ans=max(prices[i]-mi,ans);
+        }
+        return ans;
+    }
