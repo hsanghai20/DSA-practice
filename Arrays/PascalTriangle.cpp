aa728a176b68a1bf31f5a105f1c69eaa7c40aa21
hsanghai20 <harshsanghai2000@gmail.com>
2020-07-01 13:57:01 +0530

    Pascal Triangle

---
+++ Arrays/PascalTriangle.cpp
@@ -0,0 +1,20 @@
+// Make pascal triangle
+
+void pascalrow(vector<vector<int>>& arr,int n)
+    {
+        for (int i=0;i<n;i++)
+        {
+            if(i==0 || i==n-1)
+                arr[n-1].push_back(1);
+            else
+                arr[n-1].push_back(arr[n-2][i]+arr[n-2][i-1]);
+        }
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> arr(numRows);
+        for (int i=1;i<=numRows;i++)
+        {
+            pascalrow(arr,i);
+        }
+        return arr;
+    }
