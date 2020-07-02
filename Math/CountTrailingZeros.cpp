075968d7a336939309ed5a9f73d7e8981e39b7b8
hsanghai20 <harshsanghai2000@gmail.com>
2020-07-02 14:41:19 +0530

    Count trailing zeros in factorial of a number

---
+++ Math/CountTrailingZeros.cpp
@@ -0,0 +1,9 @@
+// Count trailing zeros in factorial of a number 
+
+// soln bcz only 5 and 2 make 10 2 are in large amount as compared to 5 so we just have to count no of 5 before no
+int trailingZeroes(int n) {
+        int ans=0;
+        for (long long i=5;n/i>0;i*=5)
+            ans+=n/i;
+        return ans;
+    }
