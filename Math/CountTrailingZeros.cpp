cf1c6319040f5921fd00182c5132b68d40f556c9
hsanghai20 <harshsanghai2000@gmail.com>
2020-07-02 14:41:51 +0530

    Count trailing zeros in factorial of a number

--- Math/CountTrailingZeros.cpp
+++ Math/CountTrailingZeros.cpp
@@ -1,3 +1,5 @@
+// Link: https://leetcode.com/problems/factorial-trailing-zeroes/
+
 // Count trailing zeros in factorial of a number 
 
 // soln bcz only 5 and 2 make 10 2 are in large amount as compared to 5 so we just have to count no of 5 before no
