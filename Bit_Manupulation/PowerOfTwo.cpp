10263658c09f8ca4b838931d69243520f40eee2d
hsanghai20 <harshsanghai2000@gmail.com>
2020-07-23 18:58:58 +0530

    Power Of Two

---
+++ Bit_Manupulation/PowerOfTwo.cpp
@@ -0,0 +1,8 @@
+// Link: https://leetcode.com/problems/power-of-two/
+
+// Given an integer, write a function to determine if it is a power of two.
+
+// solution:
+bool isPowerOfTwo(int n) {
+        return (n>0&&!(n&(n-1)));
+    }
