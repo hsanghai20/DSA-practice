9ab65d2754ee14ec3213e50ab86112bfb2b788d7
hsanghai20 <harshsanghai2000@gmail.com>
2020-07-31 15:49:13 +0530

    KMP algo

---
+++ String/KMP_Algo.cpp
@@ -0,0 +1,50 @@
+// Link: https://practice.geeksforgeeks.org/problems/what-is-kmp-algorithm
+
+// solution: 
+#include<bits/stdc++.h>
+using namespace std;
+// using Z algorithm or LCP
+void search(string s,int no)
+{
+  int L=0,R=0;
+  int Zarray[s.length()];
+  Zarray[0]=-1;
+  int n=s.length();
+  for(int i=1;i<s.length();i++)
+  {
+    int x;
+    if(i>=R)
+    {
+      x=0;
+    }
+    else
+    {
+      x=min(R-i+1,Zarray[i-L]);
+    }
+    while(i+x<n && s[i+x]==s[x])
+      x++;
+    Zarray[i]=x;
+    if(i+x>R)
+    {
+      L=i;
+      R=i+x;
+    }
+  }
+  for(int i=0;i<n;i++)
+  {
+    if(Zarray[i]==no)
+    {
+      cout << i-(no+1) << " ";
+    }
+  }
+}
+int main()
+{
+  string txt= "ABAAABABAAAAAABAA";
+  string pat= "ABA";
+  string s="";
+  s+=pat;
+  s+='$';
+  s+=txt;
+  search(s,pat.length());
+}
