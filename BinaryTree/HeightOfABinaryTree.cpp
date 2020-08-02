c1a026eacc257e8c6409a3497933b31ba22c5c5f
hsanghai20 <harshsanghai2000@gmail.com>
2020-08-02 22:09:41 +0530

    Height Of A Binary tree

---
+++ BinaryTree/HeightOfABinaryTree.cpp
@@ -0,0 +1,19 @@
+// Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+// solution: 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
